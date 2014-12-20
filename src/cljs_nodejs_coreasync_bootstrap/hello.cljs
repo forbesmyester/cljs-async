b390d875883efc82dec8a6f6c4fa5278e108d73d
Matthew Forrester <matthew.forrester@speechmarks.com>
2014-12-20 19:27:12 +0000

    Something working...

--- src/cljs_nodejs_coreasync_bootstrap/hello.cljs
+++ src/cljs_nodejs_coreasync_bootstrap/hello.cljs
@@ -1,19 +1,42 @@
 (ns cljs-nodejs-coreasync-bootstrap.hello
-  (:require [cljs.core.async :as async :refer [<! >! chan close! timeout]])
+  (:require [cljs.core.async :as async :refer [put! <!! >!! <! >! chan close! timeout]]
+            [cljs.nodejs :as nodejs])
   (:require-macros [cljs.core.async.macros :as m :refer [go alt!]]))
 
+(def che (nodejs/require "./asyncThing.js"))
+
+; (defn <<< [f & args]
+;   (let [c (chan)]
+;     (apply f (concat args [(fn [x]
+;                              (print (str "CB: " x))
+;                              (if (or (nil? x)
+;                                      (undefined? x))
+;                                    (close! c)
+;                                    (put! c x)) c)]))))
+
+
+(defn <<< [f & args]
+  (let [c (chan)
+        callback (fn [x]
+                   (print (str "CB: " x))
+                   (if (or (nil? x)
+                           (undefined? x))
+                     (close! c)
+                     (put! c x)))]
+    (apply f (concat args [callback])) c))
+
+(defn getCheese [x]
+  (go (<! (<<< che 5))))
+
 (defn -main [& args]
-  (go
-   (<! (timeout 200))
-   (println "world!"))
-  (println "Hello")
+  ; (print (<!! (<getCheese 5))))
+  ; (print (getCheese 5)))
+  (go (print (<! (<<< che 5)))))
+  ; (print (<!! getCheese)))
 
-  ;; node core library example
-  (cljs-nodejs-coreasync-bootstrap.fs/test "hello.js")
-
-  ;; npm library example
-  (cljs-nodejs-coreasync-bootstrap.express/test)
-  )
-
+; (let [c (chan 10)]
+;   (>!! c "hello")
+;   (print (<!! c))
+;   (close! c)))
 
 (set! *main-cli-fn* -main)
